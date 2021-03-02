fb54fb6f4a7f9bfa853368603fd6dcfe92ca8fc9
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-02 02:42:24 +0000

    Up to exercise 10

--- exercises/08-checkAge/app.js
+++ exercises/08-checkAge/app.js
@@ -1,3 +1,13 @@
 function checkAge(name, age) {
-  // your code here
+    let str = "";
+  if(age >= 21){
+      str = `Welcome, ${name}!`;;
+      return str;
+  }else{
+      str = `Go home, ${name}!`;
+    return str;
+  }
 }
+
+var output = checkAge('Adrian', 22);
+console.log(output); // --> 'Welcome, Adrian!'
