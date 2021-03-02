1f0f56c175ab729701f537bada842693f72b2946
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-02 19:53:54 +0000

    Up to exercise 70

--- exercises/80-getAverageOfElementsAtProperty/app.js
+++ exercises/80-getAverageOfElementsAtProperty/app.js
@@ -1,1 +1,26 @@
-// Write your function here
+function getAverageOfElementsAtProperty (obj, key){
+    
+    // Array.isArray evaluates if the value is an array 
+    if(Array.isArray(obj[key])){
+
+        // evaluates if passed property exist inside the object
+        if(obj.hasOwnProperty(key)){
+            let arrayLength = obj[key].length;
+
+            if(arrayLength > 0){
+                let total = 0;     
+
+                obj[key].forEach(element => total += element);
+
+                return total / arrayLength;
+            }
+        }
+    }
+    return 0;
+}
+
+var obj = {
+  key: [1, 2, 3]
+};
+var output = getAverageOfElementsAtProperty(obj, 'key');
+console.log(output); // --> 2
