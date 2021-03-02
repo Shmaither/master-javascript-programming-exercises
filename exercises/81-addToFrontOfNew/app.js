1125850feeeec4efcde8bed89f933c2b2f58a13b
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-02 20:00:58 +0000

    Up to exercise 81

--- exercises/81-addToFrontOfNew/app.js
+++ exercises/81-addToFrontOfNew/app.js
@@ -1,1 +1,11 @@
-// Write your function here
+function addToFrontOfNew (arr, element){
+    let newArray = arr.map(elem => elem);
+    
+    newArray.unshift(element);
+    return newArray;
+}
+
+var input = [1, 2];
+var output = addToFrontOfNew(input, 3);
+console.log(output); // --> [3, 1, 2];
+console.log(input); //--> [1, 2]
