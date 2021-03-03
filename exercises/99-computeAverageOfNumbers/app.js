e8ca83784f1d67b7c06d0f4d94c5a8777e8c76df
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-03 02:53:33 +0000

    Up to exercise 99

--- exercises/99-computeAverageOfNumbers/app.js
+++ exercises/99-computeAverageOfNumbers/app.js
@@ -1,1 +1,12 @@
-// Write your function here
+function computeAverageOfNumbers(arr){
+    if(arr.length > 0){
+        let total = 0;
+        arr.forEach(element => total += element);
+        return total / arr.length;
+    }
+
+}
+
+var input = [1,2,3,4,5];
+var output = computeAverageOfNumbers(input);
+console.log(output); // --> 3
