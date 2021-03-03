82c0026ecba1c1daa7b560eb32cf49277a97d61e
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-03 00:39:42 +0000

    Up to exercise 85

--- exercises/82-addToBackOfNew/app.js
+++ exercises/82-addToBackOfNew/app.js
@@ -1,1 +1,10 @@
-// Write your function here
+function addToBackOfNew (arr, num){
+    let newArr = arr.map(elem => elem);
+    newArr.push(num);
+    return newArr;
+}
+
+var input = [1, 2];
+var output = addToBackOfNew(input, 3);
+console.log(input); // --> [1, 2]
+console.log(output); // --> [1, 2, 3]
--- exercises/83-getAllElementsButNth/app.js
+++ exercises/83-getAllElementsButNth/app.js
@@ -1,1 +1,6 @@
-// Write your function here
+function getAllElementsButNth (arr, num){
+    return arr.filter((elem, index) => index !== num)
+}
+
+var output = getAllElementsButNth(['a', 'b', 'c'], 1);
+console.log(output); // --> ['a', 'c']
