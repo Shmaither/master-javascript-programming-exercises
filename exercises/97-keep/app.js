fe4c161bd91e5549cc287910c64d521cf20822c7
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-03 02:44:03 +0000

    Up to exercise 98

--- exercises/97-keep/app.js
+++ exercises/97-keep/app.js
@@ -1,1 +1,6 @@
-// Write your function here
+function keep(arr, element){
+    return arr.filter(elem => elem === element);
+}
+
+var output = keep([1, 2, 3, 2, 1], 2) 
+console.log(output); //--> [2, 2]
--- exercises/98-getOddLengthWordsAtProperty/app.js
+++ exercises/98-getOddLengthWordsAtProperty/app.js
@@ -1,1 +1,16 @@
-// Write your function here
+function getOddLengthWordsAtProperty(obj, key){
+    let array = obj[key];
+    let arrayLength = array.length;
+    if (Array.isArray(array)) {
+        if (arrayLength > 0) {
+            return array.filter(element => element % 2 !== 0);
+        }
+    } 
+    return array;
+}
+
+var obj = {
+  key: ['It', 'has', 'some', 'words']
+};
+var output = getOddLengthWordsAtProperty(obj, 'key');
+console.log(output); // --> ['has', 'words']
