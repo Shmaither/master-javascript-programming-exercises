72a6a612161d77ef31878e883bbef7cf9e84b803
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-03 02:18:11 +0000

    Up to exercise 92

--- exercises/90-getElementsLessThanOneHundredAtProperty/app.js
+++ exercises/90-getElementsLessThanOneHundredAtProperty/app.js
@@ -1,1 +1,11 @@
-// Write your function here
+function getElementsLessThan100AtProperty(obj, key){
+    let newArray = (obj[key]).filter(elem => elem < 100);
+    return newArray;
+}
+
+var obj = {
+  key: [1000, 20, 50, 500]
+};
+var output = getElementsLessThan100AtProperty(obj, 'key');
+console.log(output); // --> [20, 50]
+
--- exercises/91-countAllCharacters/app.js
+++ exercises/91-countAllCharacters/app.js
@@ -1,1 +1,20 @@
-// Write your function here
+function countAllCharacters(str){
+    let obj = {};
+
+    if(str.length > 0){
+        str.split('').forEach(element => {
+            if(obj.hasOwnProperty(element)){
+                obj[element] += 1;
+            }else{
+                obj[element] = 1;
+            }
+        });
+        return obj;
+    }else{
+        return str;
+    }
+    
+}
+
+var output = countAllCharacters('banana');
+console.log(output); // --> {b: 1, a: 3, n: 2}
